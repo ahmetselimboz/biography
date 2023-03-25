2b17371bf24197da9c2dd92180f7604565a3a85b
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2023-03-25 22:10:10 +0300

    SomethingHappend

--- js/UI.js
+++ js/UI.js
@@ -4,7 +4,7 @@ class UI {
         this.displayNav = document.querySelector('.navbar');
         this.logo = document.querySelector("#logo");
         this.githubCard = document.querySelector(".github-container");
-        this.cardLang = document.querySelector(".card-langu");
+        this.cardLang = document.querySelector(".langu");
         //this.socialCard = document.querySelector('.header-social');
         //this.socialCard.addEventListener('mouseover', this.socialCardHover.bind(this))
     }
@@ -57,7 +57,8 @@ class UI {
 
     socialCardHover(e) {
         if (e.target.id === 'social-1') {
-            this.socialCard.children[0].children[1].style.cssText = `                padding-right: 1.5rem;
+            this.socialCard.children[0].children[1].style.cssText = `                
+            padding-right: 1.5rem;
     display:block
             `;
 
@@ -65,108 +66,66 @@ class UI {
     }
 
     gitCard() {
-
-
-
         const gitApi = new githubApi();
 
         const githubInfo = gitApi.getGithubInfo();
 
         githubInfo.then((value) => {
 
-            for (let j = 0; j < 10; j++) {
+            console.log(value)
+
+            for (let j = 0; j < 4; j++) {
 
 
 
                 const lang = gitApi.getRepoLang(value[j].languages_url);
+                //var htmlUrl = gitApi.getRepoLang(value[j].url);
                 const keys = gitApi.getKeys(lang);
                 const contains = gitApi.getValue(lang);
                 var yuzdeler = [];
-                var keyaa = [];
-                var cont = [];
+
 
 
                 keys.then((key) => {
                     contains.then((val) => {
-                        //console.log(key.length)
-                        for (let i = 0; i < 1; i++) {
 
 
 
-                            for (let k = 0; k < value.length; k++) {
+                        for (let k = 0; k < value.length; k++) {
 
-                                //console.log(val.length)
+                            yuzdeler[k] = `<p>` + key[k] + `: ` + val[k] + `%</p>`
+                        }
 
-
-                                yuzdeler[k] = key[k] + `: ` + val[k] + `%`
+                        var istak = [];
+                        yuzdeler.forEach((element) => {
+                            if (element !== '<p>undefined: undefined%</p>') {
+                                istak.push(element);
 
                             }
-
-                            var istak = [];
-                            yuzdeler.forEach((element) => {
-                                if (element !== 'undefined: undefined%') {
-                                    istak.push(element);
-                                    //console.log(element)
-                                }
-
-
-
-                            });
-
-                            console.log(istak)
-
-
-                            this.githubCard.innerHTML += `
+                        });
+                        console.log(this.cardLang);
+                        var texc;
+                        //console.log(istak)
+                        this.githubCard.innerHTML += `
                                 <div class="github-col">
                                 <div class="github-cards">
                                     <h4>${value[j].name}</h4>
                                     <div class="card-langu">
                                     <div class="langu">
-                                    <p>${istak}</p>
-                                </div></div>
+                                    ${istak}
+                                    </div></div>
                                     <div class="github-card-btn">
-                                        <a class="card-btn" href="#">View</a>
+                                        <a class="card-btn" target="_blank" href="https://github.com/ahmetselimboz/${value[j].name}">View</a>
                                     </div>
                         
                                 </div>
                         
                             </div>`
 
-
-                        };
-
                     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                 })
-
-
-
             }
-
-
-
         });
-
     }
-    // ` +
-    // for(let i = 0; i < value.length; i++){
-
-    //     
-    // }
-    //    +
-    // `
 
 }
