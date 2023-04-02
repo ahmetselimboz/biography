e15aa0fb40ee4927025a486676ccede810d97c37
Ahmet Selim Boz <ahmetselimboz46@gmail.com>
2023-04-02 13:47:47 +0300

    LoadingPageIsDone

--- js/main.js
+++ js/main.js
@@ -1,37 +1,14 @@
 const altText = new UI();
 const gitApi = new githubApi();
-
-
-
 const githubInfo = gitApi.getGithubInfo();
 
 
 
-// githubInfo.then((value) => {
-//     let num = 2;
-//     const lang = gitApi.getRepoLang(value[num].languages_url);
-//     const ver = gitApi.getKeys(lang);
-
-//     ver.then((val)=> {
-//         console.log(val);
-
-        
-//     })
-    
-// });
-
 
 altText.gitCard();
 altText.myProjectCard();
-
-
-
-
-
-
 altText.navShow();
 altText.textChange();
-//const key = gitApi.getRepoLang(githubInfo);
 
-//gitApi.getKeys(key);
+
 
